4fc0adf41e99bc607a1c8c7a8586416f7238b990
Colin Osborn <colin.osborn@gmail.com>
2016-02-17 11:52:42 -0700

    accepting all

---
+++ tests/encryption_tests.rb
@@ -0,0 +1,7 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative './enigma'
+
+class EncryptionTest < Minitest::Test
+end
--- tests/enigma_tests.rb
+++ tests/enigma_tests.rb
@@ -1,9 +1,9 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative './enigma'
+require_relative '../lib/enigma'
 
-class EncryptionTest < Minitest::Test
+class EnigmaTest < Minitest::Test
   def test_message
     hello = Enigma.new
     assert_equal [7, 4, 11, 11, 14], hello.letters_to_number("hello")
@@ -12,6 +12,11 @@ class EncryptionTest < Minitest::Test
   def test_message_with_nums
     name = Enigma.new
     assert_equal [2, 14, 11, 8, 13], name.letters_to_number("colin")
+  end
+
+  def test_can_I_downcase
+    caps = Enigma.new
+    assert_equal [7, 4, 11, 11, 14], caps.letters_to_number("HELLO")
   end
 
 end
