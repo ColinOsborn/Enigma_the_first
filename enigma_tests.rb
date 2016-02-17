005a0db97dc98389a09a9582013f7fac880a7f63
Colin Osborn <colin.osborn@gmail.com>
2016-02-17 02:04:14 -0700

    tests

--- enigma_tests.rb
+++ enigma_tests.rb
@@ -8,4 +8,10 @@ class EncryptionTest < Minitest::Test
     hello = Enigma.new
     assert_equal [7, 4, 11, 11, 14], hello.letters_to_number("hello")
   end
+
+  def test_message_with_nums
+    name = Enigma.new
+    assert_equal [2, 14, 11, 8, 13], name.letters_to_number("colin")
+  end
+
 end
